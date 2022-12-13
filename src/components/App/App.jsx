b3cd86c8402975480f001a0644e71c9b09cf4edc
rastikon <40177045+rastikon@users.noreply.github.com>
2022-12-13 19:45:04 +0100

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,59 +1,65 @@
-import { Component } from 'react';
-import Form from '../Form/Form';
-import 'components/App/App.css';
-import ContactsList from '../ContactList';
-import Filter from 'components/Filter';
+import { Component } from "react";
+import Form from "../Form/Form";
+import "components/App/App.css";
+import ContactsList from "../ContactList";
+import Filter from "components/Filter";
+import { nanoid } from "nanoid";
 
 export default class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
     ],
-    filter: '',
+    filter: "",
   };
 
   // Додає контакт до списку
-  handleAddContact = newContact =>
-    this.setState(({ contacts }) => ({
-      contacts: [...contacts, newContact],
-    }));
+  handleAddContact = ({ name, number }) => {
+    const contact = {
+      id: nanoid(),
+      name,
+      number,
+    };
 
-  // Перевіряє унікальність контакту
-  handleCheckUniqueContact = name => {
     const { contacts } = this.state;
-    const isExistContact = contacts.find(contact => contact.name === name); //Якщо однакові імена поверне true, якщо ні - false, два !! і без них результат той самий
-    isExistContact && alert('Contact is already exist');
-    return !isExistContact;
+    const isExistContact = contacts.find((contact) =>
+      contact.name.toLocaleLowerCase().includes(name.toLowerCase())
+    );
+    isExistContact
+      ? alert("Contact is already exist")
+      : this.setState(({ contacts }) => ({
+          contacts: [contact, ...contacts],
+        }));
   };
 
   // Видаляє контакту
-  handleRemoveContact = id =>
+  handleRemoveContact = (id) =>
     this.setState(({ contacts }) => ({
-      contacts: contacts.filter(contact => contact.id !== id),
+      contacts: contacts.filter((contact) => contact.id !== id),
     }));
 
   // Обробник для фільтрації контактів
-  handleFilterChange = filter => this.setState({ filter });
+  handleFilterChange = (filter) => this.setState({ filter });
 
   // Метод для фільтрації контактів
   getVisibleContacts = () => {
     const { contacts, filter } = this.state;
-    return contacts.filter(contact =>
+    return contacts.filter((contact) =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
   componentDidUpdate(prevProps, prevState) {
     if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts)); //Записуєтсья в localStorage
+      localStorage.setItem("contacts", JSON.stringify(this.state.contacts)); //Записуєтсья в localStorage
     }
   }
 
   componentDidMount() {
-    const contacts = localStorage.getItem('contacts');
+    const contacts = localStorage.getItem("contacts");
     const parsedContacts = JSON.parse(contacts);
 
     // console.log(parsedContacts);
@@ -68,10 +74,7 @@ export default class App extends Component {
     return (
       <div className="form">
         <h2>Phonebook</h2>
-        <Form
-          onAdd={this.handleAddContact}
-          onCheckUnique={this.handleCheckUniqueContact}
-        />
+        <Form onAdd={this.handleAddContact} />
         <h2>Contacts List</h2>
         <Filter filter={filter} onChange={this.handleFilterChange} />
         <ContactsList
